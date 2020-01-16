cd29f12c9195f947cb1c7213a17fc6b0db2f8a98
joaoh1 <7559492+joaoh1@users.noreply.github.com>
2020-01-16 19:21:02 -0300

    Make the template bored a.k.a. personalize it for myself.

---
+++ src/main/java/io/github/joaoh1/example/ExampleMod.java
@@ -0,0 +1,10 @@
+package io.github.joaoh1.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
